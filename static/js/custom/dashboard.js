60577c87640e6f75664848480822ceb634e0f04f
Bhupen Ahuja <bhupeny2k@gmail.com>
2021-05-28 22:27:37 +0530

    changed date time

--- static/js/custom/dashboard.js
+++ static/js/custom/dashboard.js
@@ -7,7 +7,7 @@ $(function () {
             $.each(response, function(index, order) {
                 totalCost += parseFloat(order.total);
                 table += '<tr>' +
-                    '<td>'+ order.datetime.toString().split(" ")[0] +'</td>'+
+                    '<td>'+ order.datetime +'</td>'+
                     '<td>'+ order.order_id +'</td>'+
                     '<td>'+ order.customer_name +'</td>'+
                     '<td>'+ order.total.toFixed(2) +' Rs</td></tr>';
